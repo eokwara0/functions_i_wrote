7af860384d3e1384c16867e1e6647093c1b04fef
emmanuel okwara <emokwara@c4r21s2.wethinkcode.co.za>
2020-03-02 09:49:07 +0200

    str2

---
+++ str2/first_word.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+
+
+int  main(int argc, char **argv)
+{
+	int x;
+
+	x = 0;
+
+	if (argc != 1)
+	{
+			while(argv[1][x])
+			{
+				if (argc == 3)
+				{
+					break;
+				}
+			if(argv[1][x] == ' ')
+			{
+				argv[1][x] = argv[1][x+1];
+			}
+			else if (argv[1][x])
+			{
+				write(1, &argv[1][x], 1);
+				if (argv[1][x+1] == ' ')
+				{
+					break;
+				}
+				
+			}
+			++x;
+			}
+	}
+		write(1, "\n", 1);
+}
