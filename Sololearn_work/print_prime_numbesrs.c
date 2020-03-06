6229e9bcad7a3cfcfe03af114278489648b3af0a
Ck-damn <emmanuelokwara001@gmail.com>
2020-03-06 03:19:53 -0800

    solo_Learn_work

---
+++ Sololearn_work/print_prime_numbesrs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_prime_numbers(int n)
+{
+    int x;
+    int w;
+    
+    x = 0;
+    w = 2;
+ printf("%d ",w);
+    while(x  < n)
+    {
+    if (x == 1)
+    {
+        ++x;
+    }
+     else if(x%2 != 0 && x%3 != 0)
+       {
+        printf("%d ",x);
+       }
+        //printf("\n");
+        ++x;
+    }
+}
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    print_prime_numbers(num);
+    return 0;
+}
